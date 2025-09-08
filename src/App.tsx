4453e29e3ad048a64b2894099fd46e9e4a8f21c8
Marine Lamour <marine.lamour@gmx.fr>
2025-09-08 11:46:48 +0200

    Ajout de la configuration Tailwind dans VSCode et préparation du projet

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,22 @@ function App() {
   return (
     <div>
       <Header logoURL={logo}/>
+
+      <section id="about">
+        <h2>À propos</h2>
+        <p>Présentation de l'entreprise</p>
+      </section>
+
+      <section id="gallery">
+        <h2>Galerie</h2>
+        <p>Quelques photos de mes traveaux</p>
+      </section>
+
+      <section id="contact">
+        <h2>Contact</h2>
+        <p>Infos de contact</p>
+      </section>
+      
       <Footer />
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,28 @@
+import React from 'react';
+import './Header.css';
+
 type HeaderProps = {
     logoURL: string;
 };
 
-const Header =({logoURL}: HeaderProps) => {
-    return (
-        <header>
-            <img src={logoURL} alt="Logo de l'entreprise" />
-            <h1>Du Sol Au Plafond</h1>
-        </header>
-    );
+const Header: React.FC<HeaderProps> = ({ logoURL }) => {
+  return (
+    <header className='header'>
+      {/* Logo */}
+      <div className="logo">
+        <img src={logoURL} alt="Logo de l'entreprise" />
+      </div>
+
+      {/* Menu de navigation */}
+      <nav>
+        <ul>
+          <li><a href="#about">À propos</a></li>
+          <li><a href="#gallery">Galerie</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
