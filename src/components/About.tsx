ac63b90fad34d44552a33c716baf1b115e3c851c
Marine Lamour <marine.lamour@gmx.fr>
2025-09-18 21:30:57 +0200

    fix(assets): add missing image in public and update path

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,7 +8,7 @@ const About = () => {
   className="relative py-32 min-h-[60vh] bg-cover bg-center bg-no-repeat text-white flex items-center justify-center scroll-mt-20"
   style={{
     backgroundImage:
-      "url('/src/assets/todd-quackenbush-IClZBVw5W5A-unsplash.jpg')",
+      "url('/public/images/todd-quackenbush-IClZBVw5W5A-unsplash.jpg')",
   }}
 >
   {/* Overlay sombre */}
